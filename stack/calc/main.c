1fc5be06adbdaddbf93572f95225f259836958d5
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2020-09-30 12:45:04 -0300

    add main calculadora

---
+++ stack/calc/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "calculadora.h"
+
+int main(void){
+	stack_t st;
+	int i, ans, aux, peek;
+
+	st_init(&st, 5);
+
+    do{
+        ans = st_menu();
+        switch(ans){
+            case 1:
+                aux = st_calculadora(&st);
+                break;
+            case 2:
+                st_push(&st, aux);
+                break;
+            case 3:
+                peek = st_peek(&st);
+                printf("resultado anterior: %d\n", peek);
+                break;
+            case 4:
+                while(!st_empty(&st)){
+                    st_pop(&st, i);
+                    printf("pop: X\n");
+                }
+                break;
+            case 5:
+                return -1;
+        }
+    }while(ans != 5);
+
+
+	st_destroy(&st);
+
+	return 0;
+}
