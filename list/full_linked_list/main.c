4fd6e32b9b61dc024027dd669427edcce265449b
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2020-10-29 19:34:15 -0300

    add final main

--- list/full_linked_list/main.c
+++ list/full_linked_list/main.c
@@ -1,8 +1,3 @@
-// código desenvolvido por rafael b. castilhos
-// ads - ifrs, 2020. estrutura de dados - listas encadeadas
-// compilar: gcc main.c linkedlist.c linkedlist.h -o linkedlist
-// executar: ./linkedlist
-
 #include <stdio.h>
 #include "linkedlist.h"
 
@@ -24,51 +19,41 @@ int main(void){
 	struct Node *head = NULL;
     int op = 0, value = 0;
     do{
-        //imprimir lista
         print(head);
 
-        //entrada da opção do usuário
         menu();
         printf("\n-> ");
         scanf("%d", &op);
 
 		switch(op){
 			case 1:
-                //imprimir lista
                 print(head);
 				break;
 			case 2:
-                //inserir no inicio
                 printf("\ninforme elemento a ser inserido no começo: ");
                 scanf("%d", &value);
                 add_first(&head, value);
 				break;
 			case 3:
-                //inserir no final
                 printf("\ninforme elemento a ser inserido no final: ");
                 scanf("%d", &value);
                 add_last(&head, value);
                 break;
 			case 4:
-                //remover no começo
                 remove_first(&head);
                 break;
 			case 5:
-                //remover no final
                 remove_last(&head);
                 break;
 			case 6:
-                //buscar elemento
                 printf("\ninforme elemento a ser pesquisado: ");
                 scanf("%d", &value);
                 search(head, value);
                 break;
 			case 7:
-                //mostrar tamanho da lista
                 printf("\ntamanho: %d\n", length(head));
                 break;
 			case 8:
-                //imprimir 'n' primeiros elementos
                 printf("\ninforme quantos elementos deseja mostrar: ");
                 scanf("%d", &value);
                 print_nth_elements(head, value);
